1b718ddf489a858eb470307d128b3229a959b468
Diogo-Salvio <diogosalvio787@gmail.com>
2022-02-11 18:02:27 -0300

    js format

--- JS/mobile-nav-bar.js
+++ JS/mobile-nav-bar.js
@@ -21,37 +21,3 @@ btnmenu.addEventListener('click', toggleMenu);
 
 
 
-/*class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-        this.mobileMenu = document.querySelector('.nav__menu');
-        this.navList = document.querySelector('#nav__menu__itens');
-        this.navLinks = document.querySelector('#nav__menu__itens a');
-        this.activeClass = "active";
-
-        this.handleClick = this.handleClick.bind();
-    }
-
-    handleClick() {
-        console.log(this);
-        this.navList.classList.toggle(this.activeClass);
-    }
-    
-    addClickEvent() {
-        this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-
-    init() {
-        if (this.mobileMenu) {
-            this.addClickEvent();
-        }
-        return this;
-    }
-}
-
-const mobileNavbar = new MobileNavbar (
-    ".nav__menu",
-    "#nav__menu__itens",
-    "#nav__menu__itens a",
-);
-
-mobileNavbar.init();*/
